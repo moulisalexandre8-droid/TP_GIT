8cba66f9b7d0231fa6fbaaa395a11a67eeb2faaa
Alexandre <alexandre.moulis@student.junia.com>
2025-10-13 15:00:16 +0200

    Exo3

--- exo3.c
+++ exo3.c
@@ -8,28 +8,28 @@ int main() {
     scanf(" %c", &lettre);
     for(int i = 0; mot[i]!= '\0'; i++){
         if (mot[i] == lettre){
-            printf("Bien joué !");
+            printf("Bien joue !");
         }
         else{
             faute = faute + 1;
             if (faute==1)
             {
-                printf("C'est la première erreur !\n\n\n\n\n\n\n-------\n");
+                printf("C'est la premiere erreur !\n\n\n\n\n\n\n-------\n");
             }
             else if (faute==2){
-                printf("C'est la deuxième erreur !\n |\n |\n |\n |\n |\n |\n-------\n");
+                printf("C'est la deuxieme erreur !\n |\n |\n |\n |\n |\n |\n-------\n");
             }
             else if (faute==3){
-                printf("C'est la troisième erreur !-------\n | |\n |\n |\n |\n |\n-------\n");
+                printf("C'est la troisieme erreur !-------\n | |\n |\n |\n |\n |\n-------\n");
             }
             else if (faute==4){
-                printf("C'est la quatrième erreur !-------\n | |\n | O\n |\n |\n |\n-------\n");
+                printf("C'est la quatrieme erreur !-------\n | |\n | O\n |\n |\n |\n-------\n");
             }   
             else if (faute==5){
-                printf("C'est la cinquième erreur !-------\n | |\n | O\n | |\n |\n |\n-------\n");
+                printf("C'est la cinquieme erreur !-------\n | |\n | O\n | |\n |\n |\n-------\n");
             }
             else if (faute==6){
-                printf("C'est la sixième erreur !-------\n | |\n | O\n | /|\\\n |\n |\n-------\n");
+                printf("C'est la sixieme erreur !-------\n | |\n | O\n | /|\\\n |\n |\n-------\n");
             }
             else if (faute==7){
                 printf("Tu as perdu !-------\n | |\n | O\n | /|\\\n | / \\\n |\n-------\n");
@@ -40,5 +40,6 @@ int main() {
     printf("Cherche une lettre du mot");
     scanf(" %c", &lettre);
     }
+    printf("Mince c'est trop tard ! -------\n | |\n | O\n | /|\\\n | / \\\n |\n-------\n Le mot etait : %s\n", mot);
     return 0;
 }
