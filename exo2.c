18405ea65d9f779fcdfb1d11e39c7f5745af0481
Alexandre <alexandre.moulis@student.junia.com>
2025-10-13 14:38:35 +0200

    Exo2

--- exo2.c
+++ exo2.c
@@ -8,10 +8,12 @@ int main() {
     int n; //nombre dannee
     int t; //taux d'interet annuel
     printf("Entrez le montant du pret, le nombre d'annees et le taux d'interet annuel : ");
-    scanf("%d %d %d", &C, &n, &t);
-    int var1 = (C*(t/12));
-    int var2 = (1-(1+t/12)^-n*12);
-    int var_carre = pow(var2, -n*12);
+    scanf("%d ", &C);
+    scanf("%d ", &n);
+    scanf("%d ", &t);
+    float var1 = (C*(t/12));
+    float var2 = (1-(1+t/12));
+    float var_carre = pow(var2, -n*12);
     M = var1/var_carre;
     printf("Le montant de la mensuialité est de : %.2f", M);
 
