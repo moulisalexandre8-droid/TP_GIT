77f06d4090dde19ffda107877912fe61eb2ceaa2
Alexandre <alexandre.moulis@student.junia.com>
2025-11-03 14:42:05 +0100

    exec7

--- exec7.c
+++ exec7.c
@@ -1,10 +1,10 @@
 #include <stdio.h>
 
 int main() {
-    int distance_km = 100;
-    int vitesse_kmh = 50;
-    int temps = distance_km / vitesse_kmh; 
-    int minutes = temps * 60;
+    float distance_km = 100;
+    float vitesse_kmh = 50;
+    float temps = distance_km / vitesse_kmh; 
+    int minutes = (int)(temps * 60);
     printf("minutes = %d\n", minutes); 
     return 0;
-}
+} //le probleme de ce probleme est qu'on faisait une division entiere et non en float ce qui avait pour faiblesse de fausser le calcul des minutes en etant peu précis
