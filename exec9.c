9475f527715d4c6dab57b8d0fa58546c3268fc81
Alexandre <alexandre.moulis@student.junia.com>
2025-11-03 14:49:16 +0100

    exec9

--- exec9.c
+++ exec9.c
@@ -3,6 +3,6 @@
 int main() {
     int a = 10;
     int b = 20;
-    printf("a=%d b=%d\n", b); 
+    printf("a=%d b=%d\n", a, b); // ici on ne donnait que la variable b alors quon en demandait deux avec les deux %d, on a donc rajouter a
     return 0;
 }
