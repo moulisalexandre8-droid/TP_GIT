3df6759f961cc65cb98f66efbf8962fbc3ec1c0b
Alexandre <alexandre.moulis@student.junia.com>
2025-11-03 14:16:51 +0100

    exec2

--- exec2.c
+++ exec2.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 
 int main() {
-    int tab[5];
+    int tab[10]; //ici on avait defini un tableau de taille 5 ce qui est netait pas suffisant pour mettre les 10 elements demandes plus bas
     
     for (int i = 0; i < 10; i++) {
         tab[i] = i;  
