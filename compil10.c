6c171801097be96bca58ae1b0e4d0fbacba29d1e
Alexandre <alexandre.moulis@student.junia.com>
2025-11-03 14:03:35 +0100

    compil10

--- compil10.c
+++ compil10.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 
 int main() {
-    int 1abc = 5;
-    printf("%d\n", 1abc);
+    int abc1 = 5;
+    printf("%d\n", abc1); 
     return 0;
-}
+} // en C il est impossible de commencer le nom d'une variable par un chiffre ou un nombre, par contre il est possible d'en mettre un a la fin 
