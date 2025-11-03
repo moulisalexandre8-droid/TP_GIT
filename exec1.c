09a490b16ffe9651ff32582d12dab6c5a04f7ee3
Alexandre <alexandre.moulis@student.junia.com>
2025-11-03 14:13:31 +0100

    exec1

--- exec1.c
+++ exec1.c
@@ -3,7 +3,15 @@
 int main() {
     int a = 2;
     int b = 0;
-    int c = a / b;  // crash / comportement indéfini
-    printf("%d\n", c);
+    if (b == 0) {
+        printf("Erreur: division par zéro\n");
+        return 1; // quitter avec un code d'erreur
+    }
+    else {
+        int c = a / b;  // division sûre    
+        printf("%d\n", c);
+    }
+        
     return 0;
 }
+// lerreur ici est quon essaye de faire une division par 0 ce qui est impossible
