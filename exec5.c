82bdd9a3fd304afc62913d3d2ffa144e89ff4737
Alexandre <alexandre.moulis@student.junia.com>
2025-11-03 14:31:00 +0100

    exec5

--- exec5.c
+++ exec5.c
@@ -6,6 +6,7 @@ int main() {
     s[1] = 'e';
     s[2] = 'y';
     s[3] = '!';
+    s[4] = '\0'; // on met le caractere de fin de chaine pour eviter les depassements de memoire
     printf("%s\n", s); 
     return 0;
-}
+} //jai limpression que ce code marche tres bien, il renvoie bien le mot choisis danns le code et le debogage renvoie un code 0 donc sans erreur
