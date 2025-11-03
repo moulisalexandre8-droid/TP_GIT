10f976fb59d8a1a57a79a9f8da63be4bd27bab99
Alexandre <alexandre.moulis@student.junia.com>
2025-11-03 14:37:31 +0100

    exec6

--- exec6.c
+++ exec6.c
@@ -1,9 +1,11 @@
 #include <stdio.h>
 
 int main() {
-    char nom[4];
+    char nom[40]; //ici il est preferable de mettre plus de places pour stocker un nom car un caractere aditionnel peut se glicer dans notre chaine
     printf("Nom ? ");
     scanf("%s", nom); 
+    char c = 'g';
+
     printf("Bonjour %s\n", nom);
     return 0;
 }
