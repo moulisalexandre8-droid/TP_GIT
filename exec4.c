1a697c52025d819b0943df07b7fa2f5bbfb74286
Alexandre <alexandre.moulis@student.junia.com>
2025-11-03 14:21:01 +0100

    exec4

--- exec4.c
+++ exec4.c
@@ -4,6 +4,7 @@ int main() {
     int i = 0;
     while (i < 10) {
         printf("%d\n", i);
+        i++; // il n'y avait pas de compteur pour i ce qui faisait une boucle infinie
     }
     return 0;
 }
