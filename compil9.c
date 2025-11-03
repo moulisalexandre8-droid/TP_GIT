5bbc4c2e72c3fe3cf5c1e59309c87e27d4fe2f35
Alexandre <alexandre.moulis@student.junia.com>
2025-11-03 14:02:06 +0100

    compil9

--- compil9.c
+++ compil9.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 
 int main() {
-    char s[6] = "Hello";
+    char s[6] = "Hello"; // ici on a besoin d'avoir plus de place pour que "hello" rentre, il y a plus de 3 caracteres dans hello
     printf("%s\n", s);
     return 0;
 }
