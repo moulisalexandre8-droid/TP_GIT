8465182600ad5446286eacdd61a30b0570ed991a
Alexandre <alexandre.moulis@student.junia.com>
2025-11-03 14:46:33 +0100

    exec8

--- exec8.c
+++ exec8.c
@@ -2,7 +2,8 @@
 
 int main() {
     int t[3] = {10, 20, 30};
-    int i = -1;
+    int i = 0; // indice valide
     printf("%d\n", t[i]); 
-    return 0;
+    return 0; //Le probleme venait de l'utilisation d'un indice negatif t[-1], ce qui accede a une zone memoire en dehors du tableau. En C, cela peut provoquer un comportement imprevisible car le langage ne verifie pas les limites des tableaux a l'execution
+
 }
