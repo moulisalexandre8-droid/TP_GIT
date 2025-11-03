3d656043abaef4ff9e029bd967c8780dda6d6704
Alexandre <alexandre.moulis@student.junia.com>
2025-11-03 14:18:40 +0100

    exec3

--- exec3.c
+++ exec3.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 
 int main() {
-    int x;
+    int x = 0; // ici x navait pas de valeur definie ce qui donne une valeure aleatoire a celui ci et fausse le calcul
     int y = x + 5;  
     printf("%d\n", y);
     return 0;
