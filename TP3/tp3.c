96d5b4a7063771c872bcf95ddac00e1c3c2a9645
Alexandre <alexandre.moulis@student.junia.com>
2025-11-17 13:08:18 +0100

    dimensionimage

--- TP3/tp3.c
+++ TP3/tp3.c
@@ -2,9 +2,29 @@
 #include <stdio.h>
 
 int main(){
+
     int taille;
+    int width;
+    int max_height;
+
     printf("Choisis une taille compris entre 5 et 10");
     scanf("%d", taille);
+
+    if( taille < 4 && taille > 11 ){
+        printf("La taille doit etre comprise entre 5 et 10");
+        scanf("%d", taille);
+    }
+    else{
+        width = taille*taille;
+        max_height = 3*taille-1;
+
+
+
+
+
+
+
+    }
 
 
 
