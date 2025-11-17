e50ed95530ca68375fec78e295aadffcc22af501
Alexandre <alexandre.moulis@student.junia.com>
2025-11-17 13:30:09 +0100

    initialisation tableau

--- TP3/tp3.c
+++ TP3/tp3.c
@@ -6,20 +6,28 @@ int main(){
     int taille;
     int width;
     int max_height;
-    char img[taille][max_height];
+    char img[100][100];
 
     printf("Choisis une taille compris entre 5 et 10");
-    scanf("%d", taille);
+    scanf("%d", &taille);
 
     if( taille < 4 && taille > 11 ){
         printf("La taille doit etre comprise entre 5 et 10");
-        scanf("%d", taille);
+        scanf("%d", &taille);
     }
     else{
         width = taille*taille;
         max_height = 3*taille-1;
-        
+        for(int j=0; j <= width; j++){
+            for(int i=0; i <= max_height; i++){
+                img[j][i]= ' ';
+                printf("%c", img[j][i]);
+            }
+                
+        }
 
+
+        
 
 
 
