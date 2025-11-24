17f42c89ddd942c032a40aaa865ee3e3fbbc5146
Alexandre <alexandre.moulis@student.junia.com>
2025-11-24 12:59:22 +0100

    etape 1

---
+++ TP4/tp4.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+
+void afficherMenu()
+{
+    printf("==== GESTION DES NOTES ====\n");
+    printf("1.Saisir le nombre d eleve\n");
+    printf("2.Saisir les notes des eleves\n");
+    printf("3.Afficher toutes les notes\n");
+    printf("4.Afficher la moyenne d un eleve\n");
+    printf("5.Afficher la moyenne general\n");
+    printf("6.Afficher la meilleure note de chaque controle\n");
+    printf("0.Quitter\n");
+    return;
+
+
+}
+
+
+int main(){
+
+    afficherMenu();
+}
