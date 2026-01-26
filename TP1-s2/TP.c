f4835f3213dacf082c43b2d2846b2a98cbe4a721
Alexandre <alexandre.moulis@student.junia.com>
2026-01-26 15:10:46 +0100

    fin etape 1

--- TP1-s2/TP.c
+++ TP1-s2/TP.c
@@ -15,14 +15,36 @@ void lecture_donnee(int *nb_rangees, int *nb_tables, char *nom_fichier, Eleve li
     printf("Donnez moi le nom de votre fichier");
     scanf("%s", nom_fichier);
 
+    f = fopen(nom_fichier, "r");
+    if (f == NULL) {
+        printf("Erreur ouverture fichier\n");
+        return;
+    }
 
+    int i = 0;
+    int j = 0;
     char line[256];
     char nom[128], prenom[128];
     while (fgets(line, sizeof(line),f)){
         if (sscanf(line,"%127s %127s",prenom,nom )== 2 ){
-            
+            strncpy(liste[i][j].prenom, prenom, 49);
+            liste[i][j].prenom[49] = '\0';
+
+            strncpy(liste[i][j].nom, nom, 49);
+            liste[i][j].nom[49] = '\0';
+
+            j++;
+            if (j >= *nb_tables) {
+                j = 0;
+                i++;
+            }
+            if (i >= *nb_rangees) {
+                break;
+            }
         }
     }
+
+    fclose(f);
     
     
     
