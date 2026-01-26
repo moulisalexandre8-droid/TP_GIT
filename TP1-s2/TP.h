852c325e70ec88cfc1b85d5377fd74481bbc3304
Alexandre <alexandre.moulis@student.junia.com>
2026-01-26 13:10:24 +0100

    Création struct

---
+++ TP1-s2/TP.h
@@ -0,0 +1,29 @@
+#ifndef TP_H
+#define TP_H
+
+
+typedef struct 
+{
+    char prenom[50];
+    char nom[50];
+    
+} Eleve;
+
+typedef struct 
+{
+    int num_rang;
+    int num_tab;
+    int occupe;
+} Place;
+
+
+
+
+
+
+
+
+
+#endif 
+
+
