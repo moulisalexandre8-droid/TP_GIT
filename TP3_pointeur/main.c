9135dbdd33d48442a403de6940ee2bfaa3c36540
Alexandre <alexandre.moulis@student.junia.com>
2026-03-16 15:43:55 +0100

    re correction tp

--- TP3_pointeur/main.c
+++ TP3_pointeur/main.c
@@ -1,8 +1,11 @@
-#include <stdio.h>
 #include "blindtest.h"
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
 
 int main() {
-    Son songs[100];
+
+    Son song[100];
     int nb_chansons;
     int i;
     Joueur j;
@@ -10,7 +13,7 @@ int main() {
 
     srand(time(NULL));
 
-    nb_chansons = load_songs("songs.txt", songs);
+    nb_chansons = load_songs("songs.txt", song);
 
     if (nb_chansons <= 0) {
         printf("Erreur : aucune chanson chargee.\n");
@@ -23,24 +26,29 @@ int main() {
     j.score = 0;
 
     for (i = 0; i < nb_chansons; i++) {
+
         int debut = rand() % 30;
 
         printf("\n--------------------------\n");
         printf("Morceau %d sur %d\n", i + 1, nb_chansons);
         printf("Lecture de l'extrait...\n");
 
-        play_song_excerpt_at(songs[i].nom_fichier, debut, 10);
+        /* lecture du fichier mp3 */
+        play_song_excerpt_at(song[i].titre, debut, 10);
 
         printf("Entrez le titre : ");
-        scanf("%s", reponse);
+        scanf(" %[^\n]", reponse);
 
-        if (string_equals_normalized(reponse, songs[i].titre)) {
+        if (string_equals_normalized(reponse, song[i].titre)) {
+
             printf("Bonne reponse !\n");
             j.score++;
+
         } else {
+
             printf("Mauvaise reponse.\n");
-            printf("Titre attendu : %s\n", songs[i].titre);
-            printf("Artiste : %s\n", songs[i].artiste);
+            printf("Titre attendu : %s\n", song[i].titre);
+            printf("Artiste : %s\n", song[i].artiste);
         }
     }
 
