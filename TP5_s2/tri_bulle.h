ab9d53ac37ba0ddcbc02621c3f0625de6711e496
Alexandre <alexandre.moulis@student.junia.com>
2026-04-20 14:11:34 +0200

    .h fait

---
+++ TP5_s2/tri_bulle.h
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include <string.h>
+
+typedef struct {
+    int jour, mois, annee;
+} Date;
+
+typedef struct {
+    char nom[50];
+    int code;
+    Date fabrication;
+    Date peremption;
+    float prix;
+    int nb_vendus;
+    int stock;
+} Medicament;
+
+void saisir(Medicament tab[], int n);
+
+int comparerDate(Date d1, Date d2);
+
+void triBulles(Medicament tab[], int n);
